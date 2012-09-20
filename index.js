2276098e79c2547abfe235e264336aea8b4c5e5f
Ryan Day <soldair@gmail.com>
2012-09-19 18:51:52 -0700

    added support for last log position and improved performance

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var fs = require('fs')
 , mkdirp = require('mkdirp')
 , tailfd = require('tailfd').tail
 , valuefiles = require('valuefiles')
+, floody = require('floody')
 ;
 
 module.exports = function(opts){
@@ -21,6 +22,8 @@ module.exports.ForwardHo = ForwardHo;
 
 function ForwardHo(opts){
     _ext(this.options,opts||{});
+    this.checkLogPositionDir();
+    var z = this;
 }
 
 util.inherits(ForwardHo,EventEmitter);
@@ -44,6 +47,7 @@ _ext(ForwardHo.prototype,{
   connected:false,
   tailers:{},
   tail:function(logs){
+    //console.log('tail');
     var z = this;
     if(!this.tailers) this.tailers = {};
     
@@ -76,16 +80,15 @@ _ext(ForwardHo.prototype,{
   tailLog:function(log,o){
     var z = this;
 
-    console.log('tailing ',log);
+    //console.log('tailing ',log);
 
     fs.stat(log,function(err,stat){
-      //console.log('tail stat! ',err,stat);
+
       if(err && err.code != 'ENOENT') {
         console.log('error statting log ',log,err);
       } else if(!err){
         //console.log('no error ill read start position from the values file');
         return z.readLogPosition({stat:stat},function(err,pos){
-          //console.log('read log position ',err,pos);
           pos = pos||0;
           if(stat.size < pos) pos = 0;
           o.start = pos;
@@ -101,7 +104,7 @@ _ext(ForwardHo.prototype,{
       });
 
       function createTail(){
-        console.log('creating tail ',log,o);
+
         var tail = tailfd(log,o)
         , i = 0
         , lineId = null
@@ -109,15 +112,18 @@ _ext(ForwardHo.prototype,{
         ;
 
         tail.on('line',function(line,tailInfo){
-          //console.log('got a .line!');
+
           z.write(line,log,tailInfo,lineId);
           if(lineId) lineId = null;
+
         });
 
         // if theline is too long just send it as a line but tag it with line id for later reassembly
         tail.on('line-part',function(line,tailInfo){
+          
           lineId = z._id();
           z.write(line,log,tailInfo,lineId);
+
         });
         
         tail.on('error',function(){
@@ -146,6 +152,7 @@ _ext(ForwardHo.prototype,{
     return true;
   },
   connect:function(){
+    //console.log('connect');
     var z = this;
 
     if(this.connected) return;
@@ -155,7 +162,7 @@ _ext(ForwardHo.prototype,{
       this.connection = require('net').connect(opts)
         
       this.connection.on('connect',function(err,data){
-        console.log('connected ',opts);
+        //console.log('connected ',opts);
         if(z.buffer.length) {
 
           console.log('sending line buffer [',z.buffer.length,' bytes]');
@@ -191,12 +198,31 @@ _ext(ForwardHo.prototype,{
       this.connection.on('drain',function(){
         z.resumeTails();
       });
+      
+      this.floody = floody(this.connection,100,10240);
+      this.floody.on('write',function(tailInfos,bytes){
+
+          var inos = {};
+          tailInfos.forEach(function(info){
+            if(!inos[info.stat.ino]) inos[info.stat.ino] = info;
+            if(inos[info.stat.ino].logPos < info.logPos){
+              inos[info.stat.ino] = info;
+            }
+          });
+
+          Object.keys(inos).forEach(function(k){
+            z.commitLogPosition(inos[k]);
+          });
+
+          if(!z.shouldPause()) z.resumeTails();
+      });
     } else {
       //reconnect!
       this.connection.connect(opts);
     } 
   },
   reconnect:function(){
+    console.log('reconnect');
     var z = this;
     if(!this.backoff) {
       this.backoff = new Backoff({
@@ -226,30 +252,32 @@ _ext(ForwardHo.prototype,{
     }
   },
   pauseTails:function(){
-    //console.log('pausing tails!');
+
     var z = this;
     z.paused = true;
     Object.keys(z.tailers).forEach(function(k){
       z.tailers[k].pause();
     });
+
   },
   resumeTails:function(){
+
     var z = this;
     if(!z.paused) return;
-    //console.log('resuming tails!');
 
     z.paused = false;
     Object.keys(z.tailers).forEach(function(k){
       z.tailers[k].resume();
-    });   
+    });  
+
   },
   write:function(line,log,tailInfo,id){
+
     var z = this;
     line = z.format(line,log,id);
     if(z.connected) {
       z._write(line,tailInfo);
     } else {
-      console.log('disconnected. pausing');
       this.pauseTails();
       this.bufferLine(line,tailInfo);
     }
@@ -259,31 +287,29 @@ _ext(ForwardHo.prototype,{
     // 
     // just write it
     //
-    line = line instanceof Buffer?line:new Buffer(line);
 
-    var result = this.connection.write(line,function(){
-      // i have successfully transfered these bytes...
-      z.commitLogPosition(tailInfo);
-      // if i dont have too much in memory get it going again.
-      if(!z.shouldPause()) z.resumeTails();
-    });
+
+    this.floody.write(line,tailInfo);
 
     //
     // support control of the max buffer size per socket
     //
     if(z.shouldPause()) {
-      //console.log('i should pause so im pausing..');
       z.pauseTails();
     }
               
   },
   shouldPause:function(){
-    return (this.options.maxSocketBufferSize||10240) < this.connection.bufferSize;
+    var p =  (this.options.maxSocketBufferSize||10240) < this.connection.bufferSize;
+    return p;
   },
   format:function(line,log,id){
+
     var obj = {time:Date.now(),line:line,file:log};
     if(id) obj.id = id;
-    return JSON.stringify(obj)+"\n";
+    obj = JSON.stringify(obj)+"\n";
+
+    return obj;
   },
   bufferLine:function(line,tailInfo){
     console.log('buffering ',line.length,' bytes');
@@ -293,6 +319,7 @@ _ext(ForwardHo.prototype,{
   checkedDir:null,
   checkedDirQ:[],
   checkLogPositionDir:function(cb){
+    //console.log('check log pos');
     if(this.checkedDir === true) {
       return process.nextTick(function(){
         cb(null,true);
@@ -306,10 +333,15 @@ _ext(ForwardHo.prototype,{
     mkdirp(this.options.tailPositionDir,function(err){
       z.checkedDir = true;
       z.valuefiles = valuefiles(z.options.tailPositionDir);
-      while(z.checkedDirQ.length) z.checkedDirQ.shift()(err,true);
+      var _cb;
+      while(z.checkedDirQ.length) {
+        _cb = z.checkedDirQ.shift();
+        if(_cb && _cb.call) _cb(err,true);
+      }
     });
   },
   readLogPosition:function(tailInfo,cb){
+    //console.log('read log pos');
     var z = this;
     //console.log('read log position');
     z.checkLogPositionDir(function(){
@@ -321,16 +353,15 @@ _ext(ForwardHo.prototype,{
     });
   },
   commitLogPosition:function(tailInfo,cb){
+
     var z = this;
-    z.checkLogPositionDir(function(){
-      z.valuefiles.set(tailInfo.stat.ino,tailInfo.linePos,function(err,data){
-        //console.log('WROTE log position for ',tailInfo.stat.ino,data);
-        if(cb)cb(err,data);
-      });
+    z.valuefiles.set(tailInfo.stat.ino,tailInfo.linePos,function(err,data){
+      if(cb)cb(err,data);
     });
   },
   close:function(){
     this.connection.destroy();
+    this.floody.stop();
     this.connection = null;
     var z = this;
     Object.keys(this.tailers).forEach(function(log){
